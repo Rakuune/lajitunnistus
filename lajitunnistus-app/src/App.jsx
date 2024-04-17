e9e274b3a5fcb8eff148c6c06a45ba63da749e49
Rakuune <rakurantala@gmail.com>
2024-04-18 02:20:50 +0300

    Update App.jsx

--- lajitunnistus-app/src/App.jsx
+++ lajitunnistus-app/src/App.jsx
@@ -6,22 +6,6 @@ import { jsQuizz } from './constants'
 function App() {
   const [quizData, setQuizData] = useState([])
   
-  useEffect(() => {
-    console.log("This is the data: ", {jsQuizz})
-    getQuestions()
-  },[])
-
-  const getQuestions = async () => {
-    try {
-      const response = await fetch('mongodb+srv://fullstack:Jape123@cluster0.pwbmw41.mongodb.net/lajitunnistus?retryWrites=true&w=majority')
-      const data = await response.json()
-      setQuizData(data)
-      console.log(data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
   return <Quiz quizData={jsQuizz.quizData} />
 }
 
